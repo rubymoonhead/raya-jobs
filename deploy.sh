374e32c6dfa0fbcb3f6e50257006f1e8f26a318d
rayabergin <rayaberginwrites@gmail.com>
2021-06-04 19:35:21 -0700

    revised deploy.sh, vue.config.js

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,12 @@
 # abort on errors
 set -e
 
-# build
+# build to create new vue project
 npm run build
+
+# save the latest commit hash as a string
+LOGSTRING=$(git log)
+COMMIT=$(echo $LOGSTRING | awk '{print $2}')
 
 # navigate into the build output directory
 cd dist
@@ -14,12 +18,19 @@ cd dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "deploy (commit: $COMMIT)"
 
-# if you are deploying to https://<USERNAME>.github.io
+# if you're using SSH key authentication
+# git push -f git@github.com:<username>/<repo>.git master:gh-pages
+
+# if you're using HTTPS/2FA authentication
+# git push -f https://github.com/<username>/<repo>.git master:gh-pages
+git push -f https://github.com/rubymoonhead/raya.jobs.git master:gh-pages
+
+# if you're deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:rubymoonhead/raya.jobs.git master:gh-pages
+#git push -f git@github.com:rubymoonhead/raya.jobs.git master:gh-pages
 
-cd -
+cd ..
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+/*publicPath determines where Vue will try to access JS, CSS, and image files. by default the publicPath is '/'. our default github pages url is "https://<username>.github.io/<repo-name>/", so we will need to set publicPath to '/<repo-name>/'.*/
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/raya-jobs/'
