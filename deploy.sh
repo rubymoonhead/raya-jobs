c8504003de4d489c9de387bcea3982dd51219754
Raya Bergin <rayaberginwrites@gmail.com>
2021-06-04 18:21:23 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,17 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:rubymoonhead/raya-jobs.git master:gh-pages
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:rubymoonhead/raya.jobs.git master:gh-pages
 
 cd -
